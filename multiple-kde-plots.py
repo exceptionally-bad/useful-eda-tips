8f551fa1c0e7151f141d2695b4b312bb5164a05b
exceptionally-bad <109132499+exceptionally-bad@users.noreply.github.com>
2023-05-10 12:17:41 +0300

    Create multiple-kde-plots.py

---
+++ multiple-kde-plots.py
@@ -0,0 +1,17 @@
+plt.figure(figsize = (10, 12))
+
+# iterate through the sources
+for i, source in enumerate(['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']):
+    
+    # create a new subplot for each source
+    plt.subplot(3, 1, i + 1)
+    # plot repaid loans
+    sns.kdeplot(app_train.loc[app_train['TARGET'] == 0, source], label = 'target == 0')
+    # plot loans that were not repaid
+    sns.kdeplot(app_train.loc[app_train['TARGET'] == 1, source], label = 'target == 1')
+    
+    # Label the plots
+    plt.title('Distribution of %s by Target Value' % source)
+    plt.xlabel('%s' % source); plt.ylabel('Density');
+    
+plt.tight_layout(h_pad = 2.5)
